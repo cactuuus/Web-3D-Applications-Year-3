9d1b618173856bae11a7887cce7c65d44cba35f2
jacopo calvi <jacopo.bob.calvi@gmail.com>
2026-03-23 15:58:41 +0000

    added environments

---
+++ src/environments.ts
@@ -0,0 +1,50 @@
+const PATH_PREFIX = 'assets/environments/';
+
+export interface Environment {
+    name: string;
+    source: string;
+    path: string;
+}
+
+export const environments: Environment[] = [
+    {
+        name: 'Garden',
+        source: `https://polyhaven.com/a/studio_garden`,
+        path: `${PATH_PREFIX}garden.hdr`,
+    },
+    {
+        name: 'Studio',
+        source: `https://polyhaven.com/a/brown_photostudio_02`,
+        path: `${PATH_PREFIX}studio.hdr`,
+    },
+    {
+        name: 'Street',
+        source: `https://polyhaven.com/a/wide_street_01`,
+        path: `${PATH_PREFIX}street.hdr`,
+    },
+    {
+        name: 'Seaside',
+        source: `https://polyhaven.com/a/venice_sunset`,
+        path: `${PATH_PREFIX}seaside.hdr`,
+    },
+    {
+        name: 'Sunrise',
+        source: 'https://polyhaven.com/a/spruit_sunrise',
+        path: `${PATH_PREFIX}sunrise.hdr`,
+    },
+    {
+        name: 'Moonlight',
+        source: 'https://polyhaven.com/a/moonlit_golf',
+        path: `${PATH_PREFIX}moonlight.hdr`,
+    },
+    {
+        name: 'Field',
+        source: 'https://polyhaven.com/a/noon_grass',
+        path: `${PATH_PREFIX}field.hdr`,
+    },
+    {
+        name: 'Urban',
+        source: 'https://polyhaven.com/a/canary_wharf',
+        path: `${PATH_PREFIX}urban.hdr`,
+    },
+];
